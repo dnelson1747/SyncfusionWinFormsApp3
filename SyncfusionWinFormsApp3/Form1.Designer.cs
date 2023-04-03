444458beb2b4e342d9bce0f30942baa703a7a286
David Nelson <dnelson1747@gmail.com>
2023-04-03 15:23:39 -0700

    Add project files.

---
+++ SyncfusionWinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SyncfusionWinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings messageBoxSettings1 = new Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings();
+            Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings pdfViewerPrinterSettings1 = new Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings textSearchSettings1 = new Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings();
+            imageListAdv1 = new Syncfusion.Windows.Forms.Tools.ImageListAdv(components);
+            listView1 = new ListView();
+            pdfViewerControl1 = new Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 51);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(232, 408);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.ItemSelectionChanged += listView1_ItemSelectionChanged;
+            // 
+            // pdfViewerControl1
+            // 
+            pdfViewerControl1.CursorMode = Syncfusion.Windows.Forms.PdfViewer.PdfViewerCursorMode.SelectTool;
+            pdfViewerControl1.EnableContextMenu = true;
+            pdfViewerControl1.EnableNotificationBar = true;
+            pdfViewerControl1.HorizontalScrollOffset = 0;
+            pdfViewerControl1.IsBookmarkEnabled = true;
+            pdfViewerControl1.IsTextSearchEnabled = true;
+            pdfViewerControl1.IsTextSelectionEnabled = true;
+            pdfViewerControl1.Location = new Point(250, 51);
+            messageBoxSettings1.EnableNotification = true;
+            pdfViewerControl1.MessageBoxSettings = messageBoxSettings1;
+            pdfViewerControl1.MinimumZoomPercentage = 50;
+            pdfViewerControl1.Name = "pdfViewerControl1";
+            pdfViewerControl1.PageBorderThickness = 1;
+            pdfViewerPrinterSettings1.Copies = 1;
+            pdfViewerPrinterSettings1.PageOrientation = Syncfusion.Windows.PdfViewer.PdfViewerPrintOrientation.Auto;
+            pdfViewerPrinterSettings1.PageSize = Syncfusion.Windows.PdfViewer.PdfViewerPrintSize.ActualSize;
+            pdfViewerPrinterSettings1.PrintLocation = (PointF)resources.GetObject("pdfViewerPrinterSettings1.PrintLocation");
+            pdfViewerPrinterSettings1.ShowPrintStatusDialog = true;
+            pdfViewerControl1.PrinterSettings = pdfViewerPrinterSettings1;
+            pdfViewerControl1.ReferencePath = null;
+            pdfViewerControl1.ScrollDisplacementValue = 0;
+            pdfViewerControl1.ShowHorizontalScrollBar = true;
+            pdfViewerControl1.ShowToolBar = true;
+            pdfViewerControl1.ShowVerticalScrollBar = true;
+            pdfViewerControl1.Size = new Size(848, 588);
+            pdfViewerControl1.SpaceBetweenPages = 8;
+            pdfViewerControl1.TabIndex = 1;
+            pdfViewerControl1.Text = "pdfViewerControl1";
+            textSearchSettings1.CurrentInstanceColor = Color.FromArgb(127, 255, 171, 64);
+            textSearchSettings1.HighlightAllInstance = true;
+            textSearchSettings1.OtherInstanceColor = Color.FromArgb(127, 254, 255, 0);
+            pdfViewerControl1.TextSearchSettings = textSearchSettings1;
+            pdfViewerControl1.ThemeName = "Default";
+            pdfViewerControl1.VerticalScrollOffset = 0;
+            pdfViewerControl1.VisualStyle = Syncfusion.Windows.Forms.PdfViewer.VisualStyle.Default;
+            pdfViewerControl1.ZoomMode = Syncfusion.Windows.Forms.PdfViewer.ZoomMode.Default;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1127, 832);
+            Controls.Add(pdfViewerControl1);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.ImageListAdv imageListAdv1;
+        private ListView listView1;
+        private Syncfusion.Windows.Forms.PdfViewer.PdfViewerControl pdfViewerControl1;
+    }
+}
+
---
+++ SyncfusionWinFormsApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SyncfusionWinFormsApp3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTU5OTc2OUAzMjMxMmUzMTJlMzMzNUFsKzJaMDJ4SHFuUGYwaW1paWdkeEdjTURWR2F3N3FXbEo4YnRnalZidDQ9");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
